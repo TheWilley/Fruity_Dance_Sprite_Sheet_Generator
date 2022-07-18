1f30944c227a1912fda5b21aab58076f1d360d70
thewilley <thewilley25@gmail.com>
2022-07-18 18:42:05 +0200

    Working demo of preview windows!

--- shared/javascript/preview.js
+++ shared/javascript/preview.js
@@ -1,30 +1,70 @@
-// Src: https://gist.githubusercontent.com/Sahero/211c1f83d0f086ed7d4d9104d4396dff/raw/0ac42e032487e8dd356a45a2d8fdc3144d0d7f21/spritesheet.js
-var animationInterval;
-var position = 0;
+/**
+ * FPS control by elundmark
+ * https://gist.github.com/elundmark/38d3596a883521cb24f5
+ */ 
 
-function stopAnimation() {
-    clearInterval(animationInterval);
+var x = 0;
+var fps = 4;
+var now;
+var then = Date.now();
+var interval = 1000 / fps;
+var delta;
+
+function draw() {
+    // Generate settings
+    var originalCanvas = document.getElementById("canvas"),
+        startClippingX = 0,
+        startClippingY = 0,
+        clippingWidth = 80,
+        clippingHeight = 80,
+        pasteX = 0,
+        pasteY = 0,
+        pasteWidth = 80,
+        pasteHeight = 80
+
+    // Update the x variable
+    startClippingX = x;
+
+    // Get the previewCanvas
+    var previewCanvas = document.getElementById("gifPreview");
+    var context = previewCanvas.getContext("2d");
+
+    // Set the height and width
+    previewCanvas.width = document.getElementById("cell_width").value;
+    previewCanvas.height = document.getElementById("cell_height").value;
+
+    // Clear the canvas
+    context.clearRect(0, 0, previewCanvas.width, previewCanvas.height);
+
+    // Draw the image
+    context.drawImage(
+        originalCanvas,
+        startClippingX,
+        startClippingY,
+        clippingWidth,
+        clippingHeight,
+        pasteX,
+        pasteY,
+        pasteWidth,
+        pasteHeight
+    );
+
+    // Add to the x
+    x += 80;
 }
 
 function startAnimation(row) {
-    const speed = 5433; //in millisecond(ms)
+    // Call for animation
+    window.requestAnimationFrame(startAnimation);
 
-    animationInterval = setInterval(() => {
-        var spriteSheet = document.getElementById("gifPreview" + (row + 1));
-        if (cellCollection[row][position].imageSrc != null) {
-            spriteSheet.src = cellCollection[row][position].imageSrc;
-            spriteSheet.style.objectPosition = cellCollection[row][position].xOffset + "px " + cellCollection[row][position].yOffset + "px";
-            console.log(cellCollection[row][position].xOffset + "px" + cellCollection[row][position].yOffset + "px");
-        } else {
-            spriteSheet.src = '';
-        }
+    now = Date.now();
+    delta = now - then;
 
-        if (position < 7) {
-            position++;
-        } else {
-            position = 0;
-        }
-
-        //reset the position to show first sprite after the last one
-    }, speed);
+    if (delta > interval) {
+        then = now - (delta % interval);
+        draw();
+    }
+    if (x > 80 * 7) {
+        x = 0;
+    }
 }
