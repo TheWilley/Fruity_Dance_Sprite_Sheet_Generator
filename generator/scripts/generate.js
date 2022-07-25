1fdbd25fcce4481a9f524f767193eb3e06fe8c92
thewilley <thewilley25@gmail.com>
2022-07-26 00:46:22 +0200

    Tested adding more elements in BOB

--- generator/scripts/generate.js
+++ generator/scripts/generate.js
@@ -1,6 +1,7 @@
 const dom = [
     new BOB("div").cl("domElement").id("mouseCircle").s(),
-    new BOB("div").cl("hidden").insert("img").id("canvas_output").s()
+    new BOB("div").cl("hidden").i("img").id("canvas_output").s(),
+    new BOB("nav").cl("navbar navbar-expand-lg navbar-dark bg-dark").i("a.", {href: "#"}).cl("navbar-brand").st("margin-left: 10px;").co("Fruity Dance Sprite Sheet Generator").up().i("button", {"type": "button", "data-toggle": "collapse", "data-target": "#navbarSupportedContent", "aria-controls": "navbarSupportedContent", "aria-expanded": "false", "aria-label": "Toggle navigation"}).cl("navbar-toggler")
 ];
 
 dom.forEach(e => {
