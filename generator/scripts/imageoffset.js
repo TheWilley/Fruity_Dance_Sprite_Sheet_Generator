eb4d83401519a2a5a0e5afe4b63f576a0e2932af
thewilley <thewilley25@gmail.com>
2022-08-16 18:13:09 +0200

    encapsulated imageoffset  file

--- generator/scripts/imageoffset.js
+++ generator/scripts/imageoffset.js
@@ -1,85 +1,72 @@
-var img_wrapper = document.querySelector('#img_wrapper'),
-    img_container = document.querySelector('#img_container'),
-    img_element = document.querySelector('#img_element'),
-    x = 0,
-    y = 0,
-    previousObject = null,
-    mousedown = false;
+var imageOffset = function () {
+    var previousObject = null
 
-function disableElementSpecificControls(enabled) {
-    let xoffsetform = document.getElementById("offsetX");
-    let yoffsetform = document.getElementById("offsetY");
-    let remove = document.getElementById("delete");
+    function disableElementSpecificControls(enabled) {
+        let xoffsetform = document.getElementById("offsetX");
+        let yoffsetform = document.getElementById("offsetY");
+        let remove = document.getElementById("delete");
 
-    xoffsetform.disabled = enabled;
-    yoffsetform.disabled = enabled;
-    remove.disabled = enabled;
-}
-
-function show_controls(currentObject) {
-    graphicHandler.get().selectedItem = currentObject;
-    // Get input for X and Y
-    let xoffsetform = document.getElementById("offsetX");
-    let yoffsetform = document.getElementById("offsetY");
-
-    if (currentObject.getAttribute("src") == null || currentObject.getAttribute("src") == "data:,") {
-        disableElementSpecificControls(true);
-    } else {
-        disableElementSpecificControls(false);
+        xoffsetform.disabled = enabled;
+        yoffsetform.disabled = enabled;
+        remove.disabled = enabled;
     }
 
-    // Get row / cell number
-    var rownumb = currentObject.parentNode.dataset.x;
-    var cellnumb = currentObject.parentNode.dataset.y;
+    return {
+        show_controls: (currentObject) => {
+            graphicHandler.get().selectedItem = currentObject;
+            // Get input for X and Y
+            let xoffsetform = document.getElementById("offsetX");
+            let yoffsetform = document.getElementById("offsetY");
 
-    // Function variable for event
-    const setnumb = function() {
-        cellCollection[rownumb][cellnumb].xOffset = xoffsetform.value;
-        cellCollection[rownumb][cellnumb].yOffset = yoffsetform.value;
+            if (currentObject.getAttribute("src") == null || currentObject.getAttribute("src") == "data:,") {
+                disableElementSpecificControls(true);
+            } else {
+                disableElementSpecificControls(false);
+            }
 
-        graphicHandler.get().clear = true;
+            // Get row / cell number
+            var rownumb = currentObject.parentNode.dataset.x;
+            var cellnumb = currentObject.parentNode.dataset.y;
 
-        graphicHandler.redraw()
-    }
+            // Function variable for event
+            const setnumb = function () {
+                cellCollection[rownumb][cellnumb].xOffset = xoffsetform.value;
+                cellCollection[rownumb][cellnumb].yOffset = yoffsetform.value;
 
-    // Unbind all events
-    if (currentObject != previousObject) { $(xoffsetform).unbind();
-        $(yoffsetform).unbind(); }
+                graphicHandler.get().clear = true;
 
-    // First check if object has been accessed before
-    if (previousObject != null) { previousObject.style.border = "none" };
+                graphicHandler.redraw()
+            }
 
-    // Make the previous object the current one
-    previousObject = currentObject;
+            // Unbind all events
+            if (currentObject != previousObject) {
+                $(xoffsetform).unbind();
+                $(yoffsetform).unbind();
+            }
 
-    // Apply green border
-    currentObject.style.border = "green solid 3px";
+            // First check if object has been accessed before
+            if (previousObject != null) { previousObject.style.border = "none" };
 
-    // Get offset
-    if (previousObject != null) {
-        // Get stored values
-        Xoffset = cellCollection[rownumb][cellnumb].xOffset;
-        Yoffset = cellCollection[rownumb][cellnumb].yOffset;
+            // Make the previous object the current one
+            previousObject = currentObject;
 
-        // Set values
-        xoffsetform.value = Xoffset;
-        yoffsetform.value = Yoffset;
+            // Apply green border
+            currentObject.style.border = "green solid 3px";
 
-        // Add event listeners
-        $(xoffsetform).change(setnumb);
-        $(yoffsetform).change(setnumb);
-    }
-}
+            // Get offset
+            if (previousObject != null) {
+                // Get stored values
+                Xoffset = cellCollection[rownumb][cellnumb].xOffset;
+                Yoffset = cellCollection[rownumb][cellnumb].yOffset;
 
-$(function() {
-    $("body").click(function(e) {
-        // Check if click is inside the grid
-        if (!e.target.classList.contains('immg-grid') && !e.target.classList.contains('offsetControlls') ) {
-            // Disable all offsets
-            disableElementSpecificControls(true)
+                // Set values
+                xoffsetform.value = Xoffset;
+                yoffsetform.value = Yoffset;
 
-            // Remove border
-            if (graphicHandler.get().selectedItem != undefined) { graphicHandler.get().selectedItem.style.border = "none" };
+                // Add event listeners
+                $(xoffsetform).change(setnumb);
+                $(yoffsetform).change(setnumb);
+            }
         }
-    });
-})
+    }
+}
