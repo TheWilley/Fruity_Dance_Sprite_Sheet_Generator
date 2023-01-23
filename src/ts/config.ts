cf96ddeba976bf694bcd393c9cd0647c7e7c4dc6
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-01-23 18:41:15 +0100

    made config into a class

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,7 +1,7 @@
 /*/ Loading global objects here /*/
 var state = new ElementCatcher({ targetElement: document.getElementById("app"), getElementsWith: "id" });
 
-interface IConfig {
+interface IConfiguration {
             /*/ Canvas settings /*/
             maxRows: number, // Max amount of allowed rows
 
@@ -28,53 +28,57 @@ interface IConfig {
             warnBeforeLeavingPage: boolean // Warn user before leaving page to not discard any progress
 }
 
-/**
- * Configuration function
- */
-var config = function () {
-    var settings: IConfig = {
-        /*/ Canvas settings /*/
-        maxRows: 40, // Max amount of allowed rows
+class Configuration {
+    private _settings: IConfiguration
+    constructor() {
+        this._settings = {
+            /*/ Canvas settings /*/
+            maxRows: 40, // Max amount of allowed rows
+    
+            minWidth: 80, // Minimum cell width
+            minHeight: 80, // Minimum cell height
+            minXOffset: -20, // Minimum X-offset
+            minYOffset: -150, // Minimum Y-offset
+    
+            maxWidth: 150, // Maximum cell width
+            maxHeight: 150, // Maximum cell height
+            maxXOffset: 150, // Maximum X-offset
+            maxYOffset: 150, // Maximum Y-offset
+            
+            /*/ Upload settings /*/
+            maxUploadSize: "8mb", // Max image upload size
+            imageQuality: 0.7, // The image quality (1 = best quality, 0 = worst quality)
+            imageSizeMultiplier: 1, // Multiplies the max proportions of an uploaded image (by default the minWidth/minHeight and maxWidth/maxHeight values). Higher value here means better image quality.
+            maxAllowedGifFrames: 30, // Limit how many frames of a gif to export
+    
+            /*/ Other settings /*/
+            previewFPS: 4, // The FPS of a preview
+            amountOfCollections: 12, // The amount of collections 
+            background: null, // A custom background, must be a link to an image / path to a local one OR a color in HEX (null will mean default)
+            warnBeforeLeavingPage: true // Warn user before leaving page to not discard any progress
+        }
 
-        minWidth: 80, // Minimum cell width
-        minHeight: 80, // Minimum cell height
-        minXOffset: -20, // Minimum X-offset
-        minYOffset: -150, // Minimum Y-offset
-
-        maxWidth: 150, // Maximum cell width
-        maxHeight: 150, // Maximum cell height
-        maxXOffset: 150, // Maximum X-offset
-        maxYOffset: 150, // Maximum Y-offset
-        
-        /*/ Upload settings /*/
-        maxUploadSize: "8mb", // Max image upload size
-        imageQuality: 0.7, // The image quality (1 = best quality, 0 = worst quality)
-        imageSizeMultiplier: 1, // Multiplies the max proportions of an uploaded image (by default the minWidth/minHeight and maxWidth/maxHeight values). Higher value here means better image quality.
-        maxAllowedGifFrames: 30, // Limit how many frames of a gif to export
-
-        /*/ Other settings /*/
-        previewFPS: 4, // The FPS of a preview
-        amountOfCollections: 12, // The amount of collections 
-        background: null, // A custom background, must be a link to an image / path to a local one OR a color in HEX (null will mean default)
-        warnBeforeLeavingPage: true // Warn user before leaving page to not discard any progress
+        this.runConfig()
     }
 
-    new Map([
-        ["maxRows", (value: number | boolean | string) => { state.rows.setAttribute("max", value) }],
-        ["minWidth", (value) => { state.cell_width.setAttribute("min", value); state.cell_width.value = value }],
-        ["maxWidth", (value) => { state.cell_width.setAttribute("max", value) }],
-        ["minHeight", (value) => { state.cell_height.setAttribute("min", value); state.cell_height.value = value }],
-        ["maxHeight", (value) => { state.cell_height.setAttribute("max", value) }],
-        ["minXOffset", (value) => { state.offsetX.setAttribute("min", value) }],
-        ["maxXOffset", (value) => { state.offsetX.setAttribute("max", value) }],
-        ["minYOffset", (value) => { state.offsetY.setAttribute("min", value) }],
-        ["maxYOffset", (value) => { state.offsetY.setAttribute("max", value) }],
-        ["background", (value) => { let root = document.documentElement; if(value != null) String(value)[0] == "#" ? root.style.setProperty("--background", String(value)) : root.style.setProperty("--background", `url(${value})`) }],
-    ]).forEach((value, key) => {
-        if (settings[key as keyof IConfig]) value(settings[key as keyof IConfig])
-    })
-
-    return {
-        settings
+    private runConfig() {
+        new Map([
+            ["maxRows", (value: number | boolean | string) => { state.rows.setAttribute("max", value) }],
+            ["minWidth", (value) => { state.cell_width.setAttribute("min", value); state.cell_width.value = value }],
+            ["maxWidth", (value) => { state.cell_width.setAttribute("max", value) }],
+            ["minHeight", (value) => { state.cell_height.setAttribute("min", value); state.cell_height.value = value }],
+            ["maxHeight", (value) => { state.cell_height.setAttribute("max", value) }],
+            ["minXOffset", (value) => { state.offsetX.setAttribute("min", value) }],
+            ["maxXOffset", (value) => { state.offsetX.setAttribute("max", value) }],
+            ["minYOffset", (value) => { state.offsetY.setAttribute("min", value) }],
+            ["maxYOffset", (value) => { state.offsetY.setAttribute("max", value) }],
+            ["background", (value) => { let root = document.documentElement; if(value != null) String(value)[0] == "#" ? root.style.setProperty("--background", String(value)) : root.style.setProperty("--background", `url(${value})`) }],
+        ]).forEach((value, key) => {
+            if (this._settings[key as keyof IConfiguration]) value(this._settings[key as keyof IConfiguration])
+        })
     }
-}()
+
+    get settings() {
+        return this._settings
+    }
+}
