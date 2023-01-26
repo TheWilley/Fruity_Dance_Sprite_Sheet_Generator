f8eb8c70392633ca3f17bae5389b4a5574704e2d
thewilley <thewilley25@gmail.com>
2023-01-26 20:17:36 +0100

    made many changes, backup commit

--- src/ts/imageOffset.ts
+++ src/ts/imageOffset.ts
@@ -1,68 +1,74 @@
-var imageOffset = function () {
-    var previousObject = null
+import GraphicHandler from "./graphicHandler";
 
-    return {
-        /**
-         * Enables or disables the offset & delete settings
-         * @param {boolean} enabled - True: Elements are disabled; False: Elements are enabled
-         */
-        disableControls: function (enabled) {
-            state.offsetX.disabled = enabled;
-            state.offsetY.disabled = enabled;
-            state.delete.disabled = enabled;
-        },
+class imageOffset {
+    previousObject: HTMLElement = null
+    state = new Configuration().state
+    imageInfo = new ImageCollection()
+    graphicHandler = new GraphicHandler()
+    eventListeners = new EventListeners()
 
-        /**
-         * Shows controls for a cell in the table
-         * @param {object} currentObject - The target image element in the table
-         */
-        show_controls: function (currentObject) {
-            graphicHandler.setSelectedItem(currentObject);
+    /**
+     * Enables or disables the offset & delete settings
+     * @param {boolean} enabled - True: Elements are disabled; False: Elements are enabled
+     */
+    public disableControls(enabled: boolean) {
+        this.state.offsetX.disabled = enabled;
+        this.state.offsetY.disabled = enabled;
+        this.state.delete.disabled = enabled;
+    }
 
-            // Disable controls if the image src is not found
-            if (currentObject.getAttribute("src") == null) {
-                this.disableControls(true);
-            } else {
-                this.disableControls(false);
-            }
+    /**
+     * Shows controls for a cell in the table
+     * @param {object} currentObject - The target image element in the table
+     */
+    public show_controls(currentObject: HTMLElement) {
+        const self = this
 
-            // Get row / cell number
-            var rownumb = currentObject.parentNode.dataset.x;
-            var cellnumb = currentObject.parentNode.dataset.y;
+        this.graphicHandler.selectedItem = currentObject;
+        eventListeners
+        // Disable controls if the image src is not found
+        if (currentObject.getAttribute("src") == null) {
+            this.disableControls(true);
+        } else {
+            this.disableControls(false);
+        }
 
-            // Unbind all events
-            if (currentObject != previousObject) {
-                $(state.offsetX).unbind();
-                $(state.offsetY).unbind();
-            }
+        // Get row / cell number
+        var rownumb: number = Number(currentObject.parentElement.dataset.x);
+        var cellnumb: number = Number(currentObject.parentElement.dataset.y);
 
-            // Check if object has been accessed before
-            if (previousObject != null) { previousObject.style.border = "1px solid gray" };
+        // Unbind all events
+        if (currentObject != this.previousObject) {
+            $(this.state.offsetX).unbind();
+            $(this.state.offsetY).unbind();
+        }
 
-            // Make the previous object the current one
-            previousObject = currentObject;
+        // Check if object has been accessed before
+        if (this.previousObject != null) { this.previousObject.style.border = "1px solid gray" };
 
-            // Apply green border
-            currentObject.style.border = "green solid 3px";
+        // Make the previous object the current one
+        this.previousObject = currentObject;
 
-            // Get offset
-            if (previousObject != null) {
-                // Get stored values
-                Xoffset = imageInfo.getCellCollection()[rownumb][cellnumb].xOffset;
-                Yoffset = imageInfo.getCellCollection()[rownumb][cellnumb].yOffset;
+        // Apply green border
+        currentObject.style.border = "green solid 3px";
 
-                // Set values
-                state.offsetX.value = Xoffset;
-                state.offsetY.value = Yoffset;
+        // Get offset
+        if (this.previousObject != null) {
+            // Get stored values
+            const Xoffset = this.imageInfo.cellCollection[rownumb][cellnumb].xOffset;
+            const Yoffset = this.imageInfo.cellCollection[rownumb][cellnumb].yOffset;
 
-                $([state.offsetX, state.offsetY]).change(function (event) {
-                    eventListeners.checkMinMax(event);
+            // Set values
+            this.state.offsetX.value = Xoffset;
+            this.state.offsetY.value = Yoffset;
 
-                    imageInfo.getCellCollection()[rownumb][cellnumb].xOffset = state.offsetX.value;
-                    imageInfo.getCellCollection()[rownumb][cellnumb].yOffset = state.offsetY.value;
-                    graphicHandler.redraw()
-                })
-            }
+            $([this.state.offsetX, this.state.offsetY]).change(function (event) {
+                eventListeners.checkMinMax(event);
+
+                self.imageInfo.cellCollection[rownumb][cellnumb].xOffset = this.state.offsetX.value;
+                self.imageInfo.cellCollection[rownumb][cellnumb].yOffset = this.state.offsetY.value;
+                self.graphicHandler.redraw()
+            })
         }
     }
-}()
+}
