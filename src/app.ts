8aa0587f24af628751f5c74ca2300a3190893a48
thewilley <thewilley25@gmail.com>
2023-02-12 15:39:01 +0100

    removed old comment

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import "./main/sass/main.sass";
-import {init} from "./main/ts/setup";
+import { init } from "./main/ts/setup";
 
 // Init
-//FIXME: For some reason (unbeknown to the gods) this function runs **2** times
 init();
